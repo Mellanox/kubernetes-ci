f5ac9861b1c9e210f905362e4deb403f2211bfc6
abdallahyas <abdallahyas@mellanox.com>
2021-04-06 08:41:12 +0000

    Unlabel the master label of the node before deploying helm
    Following Mellanox/network-operator#159, the deployment order for
    the nic-cluster-policy changed, and the secondary network components
    are now deployed first. Because of that, the node must not have the
    `node-role.kubernetes.io/master` label to it for the multus to be
    deployed successfully. Since the scripts were waiting for the OFED
    state to become ready for it to unlabel the node, the helm check
    started to fail, but because there is a bug in the scripts that
    cause the scripts to not fail on this step, it went unnoticed.
    
    This patch unlable the node after the helm deploy command for it
    to deploy the multus successfully and not failing the deploy step.

--- nic_operator_helm/nic_operator_helm_ci_start.sh
+++ nic_operator_helm/nic_operator_helm_ci_start.sh
@@ -95,6 +95,18 @@ function deploy_operator {
         ${NIC_OPERATOR_HELM_NAME} \
         ./deployment/network-operator/
 
+    wait_pod_state "${NIC_OPERATOR_HELM_NAME}" "Running"
+    let status=$status+$?
+    if [ "$status" != 0 ]; then
+        echo "ERROR: Timeout waiting for ${NIC_OPERATOR_HELM_NAME} to become running!!"
+        popd
+        return $status
+    fi
+
+    sleep 5
+
+    unlabel_master
+
     wait_nic_policy_states "" "state-ofed"
     if [ "$?" != 0 ]; then
         echo "Timed out waiting for operator to become running"
