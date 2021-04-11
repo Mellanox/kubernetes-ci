7cfac02a52b299289e03a9e0ead4303624784c08
Mamduh Alassi <mamduhala@nvidia.com>
2021-04-11 10:00:42 +0300

    Add deploy kind cluster
    --num-workers number of cluster workers
    --kind-config kind config file to use, when used ignores num-workers
    --kubeconfig kubeconfig for cluster
    
    Signed-off-by: Mamduh Alassi <mamduhala@nvidia.com>

--- run_kind_ci.sh
+++ run_kind_ci.sh
@@ -9,11 +9,16 @@ SUPPORTED_PHASES=("prepare-ci-environment" "deploy-kind")
 export PHASES_TO_RUN=("${SUPPORTED_PHASES[@]}")
 
 usage() {
-  echo "usage: run_kind_ci.sh [[--project <name>] [--skip-phases <phases>] [-h]]"
+  echo "usage: run_kind_ci.sh [[--project <name>] [--skip-phases <phases>] [--num-workers <num>]"
+  echo "                       [--kind-config <conf-fil>] [--kubeconfig <path>]  [-h]]"
   echo ""
   echo "--project              Project to run CI: ${KIND_CI_PROJECTS[*]}"
   echo "                       Required field"
   echo "--skip-phases          Comma separated phases, phases: ${KIND_CI_PHASES[*]}"
+  echo "--num-workers          Number of worker nodes. DEFAULT: 2 worker"
+  echo "--kind-config          Kind configuration file, if provided skip rendering related parameters"
+  echo "                       if provided, ignores rendering related parameters (num-workers)"
+  echo "--kubeconfig           KUBECONFIG for kind cluster"
   echo ""
 }
 
@@ -65,6 +70,29 @@ parse_args() {
       shift
       parse_skip_phases $1
       ;;
+    --num-workers)
+      shift
+      if ! [[ "$1" =~ ^[0-9]+$ ]]; then
+        echo "Invalid num-workers: $1"
+        usage
+        exit 1
+      fi
+      export KIND_NUM_WORKER=$1
+      ;;
+    --kind-config)
+      shift
+      kind_conf=$1
+      if test ! -f "$kind_conf"; then
+        echo "$kind_conf does not  exist"
+        usage
+        exit 1
+      fi
+      export KIND_CONFIG=$1
+      ;;
+    --kubeconfig)
+      shift
+      export KUBECONFIG=$1
+      ;;
     -h | --help)
       usage
       exit
@@ -88,6 +116,8 @@ parse_args() {
 set_default_params() {
   # Set default values
   export WORKSPACE=${WORKSPACE:-"/tmp/kind_ci/$PROJECT"}
+  export KIND_NUM_WORKER=${KIND_NUM_WORKER:-2}
+  export KUBECONFIG=${KUBECONFIG:-$HOME/admin.conf}
 }
 
 print_params() {
@@ -96,6 +126,9 @@ print_params() {
   echo "WORKSPACE       = ${WORKSPACE}"
   echo "PHASES_TO_RUN   = ${PHASES_TO_RUN[*]}"
   echo "PROJECT         = ${PROJECT}"
+  echo "KIND_NUM_WORKER = ${KIND_NUM_WORKER}"
+  echo "KIND_CONFIG     = ${KIND_CONFIG}"
+  echo "KUBECONFIG      = ${KUBECONFIG}"
   echo ""
 }
 
